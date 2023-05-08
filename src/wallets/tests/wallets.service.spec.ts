98af0590bc47c9fdc1244018bfadd9ee21909a71
Jimbo <majidshahbani@gmail.com>
2023-05-08 23:32:40 +0330

    feat(wallets): add wallets service
    Signed-off-by: Jimbo <majidshahbani@gmail.com>

---
+++ src/wallets/tests/wallets.service.spec.ts
@@ -0,0 +1,60 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { WalletsService } from '../wallets.service';
+import { WALLETS_SERVICE_TOKEN } from '../interfaces/wallets.service';
+import {
+  IWalletsRepository,
+  WALLETS_REPOSITORY_TOKEN,
+} from '../interfaces/wallets.repository';
+
+class MockedRepository implements IWalletsRepository {
+  private wallet = new Map();
+
+  async addMoney(userId: number, amount: number): Promise<number> {
+    const currentValue = await this.getBalance(userId);
+    this.wallet.set(userId, currentValue + amount);
+    return currentValue + amount;
+  }
+
+  async getBalance(userId: number): Promise<number> {
+    return this.wallet.get(userId) ?? 0;
+  }
+}
+
+describe('BalancesService', () => {
+  let service: WalletsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: WALLETS_SERVICE_TOKEN,
+          useClass: WalletsService,
+        },
+        {
+          provide: WALLETS_REPOSITORY_TOKEN,
+          useClass: MockedRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<WalletsService>(WALLETS_SERVICE_TOKEN);
+  });
+
+  describe('addMoney', () => {
+    it('should initiate first money in the wallet', async () => {
+      const addedMoney = 10_000_000;
+      const expectedMoney = 10_000_000;
+      const balance = await service.addMoney(12, addedMoney);
+      expect(balance).toEqual(expectedMoney);
+    });
+    it('should throw error when user has not enough money', async () => {
+      const addedMoney = 10_000_000;
+      const subtractedMoney = -20_000_000;
+      const balance = await service.addMoney(12, addedMoney);
+      expect(balance).toEqual(addedMoney);
+      await expect(service.addMoney(12, subtractedMoney)).rejects.toThrowError(
+        'Invalid amount',
+      );
+    });
+  });
+});
--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { IWalletsService } from './interfaces/wallets.service';
+import {
+  IWalletsRepository,
+  WALLETS_REPOSITORY_TOKEN,
+} from './interfaces/wallets.repository';
 
 @Injectable()
-export class WalletsService {}
+export class WalletsService implements IWalletsService {
+  constructor(
+    @Inject(WALLETS_REPOSITORY_TOKEN)
+    private readonly repository: IWalletsRepository,
+  ) {}
+
+  async addMoney(userId: number, amount: number): Promise<number> {
+    const balance = await this.getBalance(userId);
+    const isValidTransaction = balance + amount >= 0;
+    if (isValidTransaction) return this.repository.addMoney(userId, amount);
+    throw new Error('Invalid amount: Not enough money in your wallet.');
+  }
+
+  getBalance(userId: number): Promise<number> {
+    return this.repository.getBalance(userId);
+  }
+}
