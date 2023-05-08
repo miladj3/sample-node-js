1e1401bd7971d529bf0eb4cfe98bdb9e604b350a
Majeed Shahbani <shahbani@gmail.com>
2023-05-08 20:37:41 +0330

    feat(transactions): add transactions module
    Signed-off-by: Majeed Shahbani <shahbani@gmail.com>

--- src/user-wallet/user-wallet.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BalancesServiceService } from './balances-service/balances-service.service';
-import { TransactionsServiceService } from './transactions-service/transactions-service.service';
-
-@Module({
-  providers: [BalancesServiceService, TransactionsServiceService],
-})
-export class UserWalletModule {}
