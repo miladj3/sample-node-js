5d8b59001eeea5aec80e2e98cfa610ff69da3a1e
Majeed Shahbani <shahbani@gmail.com>
2023-05-08 19:25:16 +0330

    refactor(dependencies): make adjustments to the app to be in microservice app
    Signed-off-by: Majeed Shahbani <shahbani@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as process from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: process.env.APP_TRANSPORTER_HOST,
+        port: Number(process.env.APP_TRANSPORTER_PORT),
+        retryDelay: 3_000,
+      },
+    },
+  );
+  await app.listen();
 }
+
 bootstrap();
