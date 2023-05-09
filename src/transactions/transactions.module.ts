e89fc93c5860a603f465f93c528418ca15c606c6
Jimbo <majidshahbani@gmail.com>
2023-05-09 20:07:11 +0330

    refactor(core): change project structure to monorepo
    Signed-off-by: Jimbo <majidshahbani@gmail.com>

--- src/transactions/transactions.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TransactionsService } from './transactions.service';
-
-@Module({
-  providers: [TransactionsService],
-})
-export class TransactionsModule {}
